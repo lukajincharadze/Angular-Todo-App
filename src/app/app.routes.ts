67d48eed2410f0ec5346aa3e44caf5b42ae1b062
Luka Jincharadze <lukajinchara@gmail.com>
2024-11-09 14:30:10 +0400

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,31 @@
 import { Routes } from '@angular/router';
+import { TaskCreationComponent } from './task-creation/task-creation.component';
+import { TaskListComponent } from './task-list/task-list.component';
 
 export const routes: Routes = [
+  // {
+  //   path: 'create',
+  //   loadComponent: () =>
+  //     import('./task-creation/task-creation.component').then(
+  //       (m) => m.TaskCreationComponent
+  //     ),
+  // },
+
+  // {
+  //   path: 'list',
+  //   loadComponent: () =>
+  //     import('./task-list/task-list.component').then(
+  //       (m) => m.TaskListComponent
+  //     ),
+  // },
+
   {
     path: 'create',
-    loadComponent: () =>
-      import('./task-creation/task-creation.component').then(
-        (m) => m.TaskCreationComponent
-      ),
+    component: TaskCreationComponent,
   },
   {
     path: 'list',
-    loadComponent: () =>
-      import('./task-list/task-list.component').then(
-        (m) => m.TaskListComponent
-      ),
+    component: TaskListComponent,
   },
   { path: '', redirectTo: '/create', pathMatch: 'full' },
   { path: '**', redirectTo: '/list' },
